717061fe297e339b4c1f18cfbcad549fa6d443eb
ljy <3165825953@qq.com>
2022-11-22 17:47:53 +0800

    第一周

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package children;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Demo {
+public static void main(String[] args) {
+ArrayList<Student> array=new ArrayList<Student>();
+Scanner in=new Scanner(System.in);
+Child a=new Child(); 
+a.SchoolSystem(10,15,20); 
+while(a.addStudent(array)){
+ a.addStudent(array);
+}
+System.out.println("班级学生信息");
+a.print(array);
+}
+}
